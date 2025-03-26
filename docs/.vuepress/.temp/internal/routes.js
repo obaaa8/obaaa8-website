82e067f1b1c5ef410a38a5ff4bcacb4e7e406004
محمود عبدالسميع التوم <obaaa8@gmail.com>
2025-03-26 13:25:33 +0300

    Delete docs/.vuepress/.temp directory

--- docs/.vuepress/.temp/internal/routes.js
+++
@@ -1,6 +0,0 @@
-export const redirects = JSON.parse("{}")
-
-export const routes = Object.fromEntries([
-  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"/home/mahmoud/المستندات/obaaa8/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"Hello VuePress"} }],
-  ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"/home/mahmoud/المستندات/obaaa8/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
-]);
