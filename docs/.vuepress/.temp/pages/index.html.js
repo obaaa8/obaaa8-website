4d090f2c1bdc5b527b2022b089d7015648faa09c
obaaa8 <obaaa8@gmail.com>
2025-03-26 13:13:35 +0300

    test

---
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -0,0 +1,3 @@
+import comp from "/home/mahmoud/المستندات/obaaa8/docs/.vuepress/.temp/pages/index.html.vue"
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"Hello VuePress\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{},\"filePathRelative\":\"README.md\"}")
+export { comp, data }
